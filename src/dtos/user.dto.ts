c4279c1b9ad4d3aba7ca9afa48e4cee5ccbdb3a3
Unisha <unishakhanal3@gmail.com>
2026-06-09 17:01:41 +0545

    dtos added

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,4 @@
+// Note: Use types from user.type.ts instead. These DTOs are kept for backward compatibility only.
+// RegisterDTO and LoginDTO are defined in user.type.ts
+
+export type { RegisterDTO, LoginDTO } from "../types/user.type";
